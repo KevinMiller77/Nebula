1a6329d84d36248ec53b07c43fa8646225bff719
Kevin Miller <kevinamiller77@gmail.com>
2022-10-21 00:37:18 -0700

    Submodules are pulling correctly, use static glad files instead due to simplicity

---
+++ Nebula/ext/glad/premake5.lua
@@ -0,0 +1,31 @@
+project "glad"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "../../include/glad/glad.h",
+        "../../include/KHR/khrplatform.h",
+        "src/glad.c"
+    }
+
+    sysincludedirs
+    {
+        "../../include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
